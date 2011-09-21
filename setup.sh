927e2a6f18cf32cadf55bb889e0af8a344f959b4
David Terence Sadler <davidtsadler@googlemail.com>
2011-09-21 15:51:22 +0200

    Generate SSH key.
    A SSH key is genereated if one doesn't exist. The public SSH key is also copied to the clipboard and the default browser opened to allow the key to be pasted into a github account.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 #A Bash script for setting up a development enviroment on my computer.
 
@@ -6,6 +6,9 @@ ROOT_UID=0  # UID for root users
 E_ROOT=1  # Root exit error
 
 PROJECTS_DIR="$HOME/projects"
+
+# Ask GNOME for the web browser command.
+BROWSER=`gconftool-2 --get '/desktop/gnome/url-handlers/http/command' | cut -f1 -d' ' `
 
 if [[ $EUID -eq 0 ]]; then
   echo "You cannot run this script as root!"
@@ -18,6 +21,25 @@ if [[ -d "$PROJECTS_DIR" ]]; then
 else
   echo "Creating directory $PROJECTS_DIR"
   mkdir "$PROJECTS_DIR"
+fi
+
+# Using apt-get so ensure it is up to date first.
+sudo apt-get update -y
+
+echo "Installing packages."
+sudo apt-get install xclip -y
+
+if [[ -f "$HOME/.ssh/id_rsa.pub" ]]; then
+  echo "Skipping generation of SSH key as one already exists."
+else
+  echo "Generating SSH key."
+  ssh-keygen -t rsa
+fi
+
+if [[ -f "$HOME/.ssh/id_rsa.pub" ]]; then
+  echo "Copying SSH public key to the clipboard. Paste it into your Github account."
+  cat "$HOME/.ssh/id_rsa.pub" | xclip
+  "$BROWSER" https://github.com/account/ssh
 fi
 
 exit 0
